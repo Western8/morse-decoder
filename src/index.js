7ee6bf913c0a3f441099e76ea1dab217ea3606c3
Western8 <westernchrome@gmail.com>
2023-01-21 00:32:23 +0300

    feat: add func decode

--- src/index.js
+++ src/index.js
@@ -38,9 +38,44 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let arrMorse = [];
+    
+    let len = expr.length / 10;
+    for (i = 0; i < len; i++) {
+      let letMorse = [];
+      for (k = 0; k < 10; k += 2) {
+        letMorse.push(expr.slice(i*10+k, i*10+k + 2));
+      }
+      arrMorse.push(letMorse);
+    }
+
+    arrMorse = arrMorse.map(function(item) {
+        return item.map(function(item2) {
+            switch (item2) {
+                case '00' : return ''
+                case '10' : return '.'
+                case '11' : return '-'
+                case '**' : return '**'
+                default: return item2
+            }
+        })
+    })
+
+    arrMorse = arrMorse.map(item => item.join(''));
+    let str = arrMorse.map(item => (item === '**********') ? ' ': MORSE_TABLE[item]).join('');
+
+    return str;
 }
 
 module.exports = {
     decode
 }
+
+/* 
+n = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010";
+console.log(n);
+//res = module.exports(n);
+res = decode(n);
+console.log(res);
+ */
